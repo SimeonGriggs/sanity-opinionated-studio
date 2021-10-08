d51857474dd9d41c667a2fa8304962fb1257fd98
Simeon Griggs <simeon@hey.com>
2021-10-08 09:45:54 +0100

    remove fancy desk structure, upgrade sanity

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,15 +1,10 @@
 import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
-// eslint-disable-next-line import/no-unresolved
-import * as Documents from './documents'
-// eslint-disable-next-line import/no-unresolved
-import * as Objects from './objects'
+import article from './documents/article'
+import seo from './objects/seo'
 
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([
-    ...Object.keys(Documents).map((key) => Documents[key]),
-    ...Object.keys(Objects).map((key) => Objects[key]),
-  ]),
+  types: schemaTypes.concat([article, seo]),
 })
